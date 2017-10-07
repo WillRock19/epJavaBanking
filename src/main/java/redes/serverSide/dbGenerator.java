85bd592db545ec601e62198f920403284f57b428
William Pinto <william.pinto@lambda3.com.br>
2017-10-07 08:39:15 -0300

    Criar e popular BD funcionando com sucesso.

--- src/main/java/redes/serverSide/dbGenerator.java
+++ src/main/java/redes/serverSide/dbGenerator.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
@@ -48,23 +50,28 @@ public class dbGenerator
 	
 	public void CreateTablesIfNotExists() 
 	{
-		String createUserTable = createUserTableStatement();
 		String createAccountTable = createAccountTableStatement();
+		String createUserTable = createUserTableStatement();
 		
 		try
 		{
+			System.out.println("Criando tabelas...");
+			
 			if(connection == null)
 				OpenDataBaseConnection();
 			
 			Statement statement = connection.createStatement();
             
-			statement.execute(createUserTable);
 			statement.execute(createAccountTable);
+			statement.execute(createUserTable);
+			
+			System.out.println("Tabelas criadas com sucesso!");
         } 
 		catch (SQLException e) 
 		{
 			System.err.println("Nao foi possivel criar as tabelas do banco de dados.");
             System.err.println("O seguinte erro ocorreu: " + e.getMessage());
+            System.exit(0);
         }
 	}
 	
@@ -72,42 +79,26 @@ public class dbGenerator
 	{
 		try
 		{
+			System.out.println("Populando Banco de dados...");
+			
 			if(connection == null)
 				OpenDataBaseConnection();
 			
 			Statement statement = connection.createStatement();
             
-			if(!UserTableHasValues(statement)) 
-			{
-				String usuario1 = "Paulo Roberto Silva Freitas";
-				String usuario2 = "Marcos da Silva Teixeira";
-				
-				for(int index = 0; index < 2; index++) 
-				{
-					String sql = createUserInsertStatement();
-					PreparedStatement prepared = connection.prepareStatement(sql);
-					
-					prepared.setString(1, usuario1);
-					prepared.setString(2, "senha1");
-				
-					prepared.setInt(3, index + 1);
-					prepared.setString(4, "Rua Long Beach 34" + index);
-					//prepared.setDate(5, new Date());
-				}
-				
-				
-				
-			}
-			
 			if(!AccountTableHasValues(statement)) 
-			{
-				String sql = createAccountInsertStatement();
-			}
+				createTwoDefaultAccounts();
+			
+			if(!UserTableHasValues(statement)) 
+				createTwoDefaultUsers();
+			
+			System.out.println("Banco populado com sucesso!");
         } 
 		catch (SQLException e) 
 		{
 			System.err.println("Nao foi possivel popular o Banco de Dados.");
             System.err.println("O seguinte erro ocorreu: " + e.getMessage());
+            System.exit(0);
         }
 	}
 	
@@ -121,40 +112,37 @@ public class dbGenerator
         Class.forName("org.sqlite.JDBC");
 	}
 
-	
 	private String createUserTableStatement() 
 	{
-		return "CREATE TABLE IF NOT EXISTS User(\n"
-			     + "Id integer NOT NULL AUTO_INCREMENT, \n"
+		return "CREATE TABLE IF NOT EXISTS User (\n"
+			     + "Id integer PRIMARY KEY, \n"
 			     + "UserName text NOT NULL, \n"
 			     + "Password text NOT NULL, \n"
 			     + "AccountId integer NOT NULL, \n"
 			     + "Adress text NOT NULL,       \n"
 			     + "Birth_Date DATE NOT NULL,   \n"
-			     + "Age tinyint NOT NULL   		\n "
-			     + "PRIMARY KEY (Id)"
+			     + "Age tinyint NOT NULL,  		\n "
 			     + "FOREIGN KEY (AccountId) REFERENCES Account(Id)"
-			     + ")";
+			     + ");";
 	}
 	
 	private String createAccountTableStatement() 
 	{
-		return "CREATE TABLE IF NOT EXISTS Account(\n"
-					+ "Id integer  NOT NULL AUTO_INCREMENT,  \n"
+		return "CREATE TABLE IF NOT EXISTS Account (\n"
+					+ "Id integer PRIMARY KEY ,  \n"
 					+ "Open_Date DATE NOT NULL, \n"
-					+ "Ammount_Money integer NOT NULL, \n"
-					+ "PRIMARY KEY (Id)"
-				    + ")";
+					+ "Ammount_Money double NOT NULL \n"
+				    + ");";
 	}
 
 	private String createUserInsertStatement() 
 	{
-		return "INSERT INTO Usuario(UserName, Password, AccountId, Adress text, Birth_Date, Age) VALUES(?,?,?,?,?,?)";	
+		return "INSERT INTO User(Id, UserName, Password, AccountId, Adress, Birth_Date, Age) VALUES(?,?,?,?,?,?,?)";	
 	}
 	
 	private String createAccountInsertStatement() 
 	{
-		return "INSERT INTO Account(Open_Date, Ammount_Money) VALUES(?,?)";	
+		return "INSERT INTO Account(Id, Open_Date, Ammount_Money) VALUES(?,?,?)";	
 	}
 	
 	private boolean UserTableHasValues(Statement statement) throws SQLException
@@ -174,6 +162,73 @@ public class dbGenerator
 		
 		return result.next();
 	}
+	
+	private void createTwoDefaultAccounts() throws SQLException
+	{	
+		String accountDefaultData[][] = new String[][] 
+		{ 
+			{ "22-04-2015 08:40:00", "4025.56" }, 
+			{ "19-01-2016 12:30:56", "800.00", } 
+		};
+		
+		for(int index = 0; index < 2; index++) 
+		{
+			String sql = createAccountInsertStatement();
+			PreparedStatement prepared = connection.prepareStatement(sql);
+			
+			String dateInString = accountDefaultData[index][0];
+			
+			prepared.setInt(1, index+1);
+			prepared.setDate(2, DateToInsert(dateInString));
+			prepared.setDouble(3, Double.parseDouble(accountDefaultData[index][1]));
+			
+			prepared.executeUpdate();
+		}
+	}
 
-
+	private void createTwoDefaultUsers() throws SQLException
+	{
+		String userDefaultData[][] = new String[][] 
+		{ 
+			{ "Paulo Roberto Silva Freitas", "19-01-1999 00:00:00"}, 
+			{ "Marcos da Silva Teixeira", "19-06-2000 00:00:00"} 
+		};
+		
+		for(int index = 0; index < 2; index++) 
+		{
+			String sql = createUserInsertStatement();
+			PreparedStatement prepared = connection.prepareStatement(sql);
+			
+			prepared.setInt(1, index + 1);
+			prepared.setString(2, userDefaultData[index][0]);
+			prepared.setString(3, "senha1");
+		
+			prepared.setInt(4, index + 1);
+			prepared.setString(5, "Rua Long Beach 34" + index);
+			
+			String dateInString = userDefaultData[index][1];
+			
+			prepared.setDate(6, DateToInsert(dateInString));
+			prepared.setInt(7, 18 + index);
+			
+			prepared.executeUpdate();
+		}
+	}
+	
+	private java.sql.Date DateToInsert(String dateInString)
+	{
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+			Date birth_date = sdf.parse(dateInString);
+			
+			return new java.sql.Date(birth_date.getTime());
+		}
+		catch(ParseException e) 
+		{
+			System.err.println("Nao foi possivel criar uma data para inserir no Banco de Dados.");
+			System.err.println("O seguinte erro ocorreu: " + e.getMessage());
+			
+			return null;
+		}
+	}
 }
